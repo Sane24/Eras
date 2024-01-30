13caf34489d420feb8e9d5bf334f4b3c85806a9f
Sane24 <sampurn.bhowmick@berkeley.edu>
2024-01-29 19:51:54 -0800

    merch page

--- swift.js
+++ swift.js
@@ -2,28 +2,26 @@
 document.addEventListener("DOMContentLoaded", function () {
     const merchGrid = document.getElementById("merchGrid");
     const merchItems = [
-        { image: 'merch1.jpg', name: 'Item 1', description: 'Description of the item.' },
-    { image: 'merch2.jpeg', name: 'Item 2', description: 'Description of the item.' },
-    { image: 'merch3.jpg', name: 'Item 3', description: 'Description of the item.' },
-    { image: 'merch4.jpg', name: 'Item 4', description: 'Description of the item.' },
-    { image: 'merch5.jpg', name: 'Item 5', description: 'Description of the item.' },
-    { image: 'merch6.jpg', name: 'Heart Photo Hoodie', description: 'Description of the item.' },
-    { image: 'merch7.jpeg', name: 'Heather Gray Crewneck', description: 'Description of the item.' },
-    { image: 'merch8.jpg', name: 'Item 8', description: 'Description of the item.' },
-    { image: 'merch9.jpg', name: 'Item 9', description: 'Description of the item.' },
-    { image: 'merch10.jpg', name: 'Item 10', description: 'Description of the item.' },
-    { image: 'merch11.jpg', name: 'Item 11', description: 'Description of the item.' },
-    { image: 'merch12.jpg', name: 'Item 12', description: 'Description of the item.' },
-    { image: 'merch13.jpg', name: 'Item 13', description: 'Description of the item.' },
-    { image: 'merch14.jpg', name: 'Item 14', description: 'Description of the item.' },
-    { image: 'merch 18.jpg', name: 'Oversized T-Shirt', description: 'Description of the item.' },
-    { image: 'merch16.jpg', name: 'Item 16', description: 'Description of the item.' },
-    { image: 'merch17.jpg', name: 'Item 17', description: 'Description of the item.' },
-    { image: 'merch18.jpg', name: 'Item 18', description: 'Description of the item.' },
-    { image: 'merch19.jpg', name: 'Item 18', description: 'Description of the item.' },
-    { image: 'merch20.jpg', name: 'Item 20', description: 'Description of the item.' },
-
-
+        { image: 'merch1.jpg', name: 'Black T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch2.jpeg', name: 'White Crewneck', description: 'Available in S, M, L, XL <br> $65' },
+    { image: 'merch3.jpg', name: 'Black T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch4.jpg', name: 'I Just Want To Stay Crewneck', description: 'Available in S, M, L, XL <br> $65' },
+    { image: 'merch5.jpg', name: 'Collage Black Long Sleeve', description: 'Available in S, M, L, XL <br> $55' },
+    { image: 'merch6.jpg', name: 'Heart Photo Hoodie', description: 'Available in S, M, L, XL <br> $75' },
+    { image: 'merch7.jpeg', name: 'Heather Gray Crewneck', description: 'Available in S, M, L, XL <br> $65' },
+    { image: 'merch8.jpg', name: 'Gray Crewneck', description: 'Available in S, M, L, XL <br> $65' },
+    { image: 'merch9.jpg', name: 'Collage White Hoodie', description: 'Available in S, M, L, XL <br> $75' },
+    { image: 'merch10.jpg', name: 'Live Photo Stars T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch11.jpg', name: '1989 Album T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch12.jpg', name: 'Reputation Album T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch13.jpg', name: 'Cropped Beige Pullover', description: 'Available in S, M, L, XL <br> $65' },
+    { image: 'merch14.jpg', name: 'RED (Taylor Version) Album T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch 18.jpg', name: 'Oversized T-Shirt', description: 'Available in S, M, L <br> $45' },
+    { image: 'merch16.jpg', name: 'The Eras Tour Poster', description: '$30' },
+    { image: 'merch17.jpg', name: 'US Dates Black Hoodie', description: 'Available in S, M, L, XL <br> $75' },
+    { image: 'merch18.jpg', name: 'Fearless (Taylor Version) Album T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch19.jpg', name: 'Midnights Album T-Shirt', description: 'Available in S, M, L, XL <br> $45' },
+    { image: 'merch20.jpg', name: 'Guitar Pick-Shaped Coasters', description: '$35' },
     ];
 
     merchItems.forEach(item => {
@@ -40,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
             </div>
         `;
-
         merchGrid.appendChild(merchItem);
     });
+
 });
